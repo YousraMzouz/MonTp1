eaf843861726811d4c45201c2aea6933353b1d1c
NouhailaRhimini <65727538+NouhailaRhimini@users.noreply.github.com>
2021-03-19 23:29:13 +0100

    Update HelloDevops.java

--- TP1-GIT/src/HelloDevops.java
+++ TP1-GIT/src/HelloDevops.java
@@ -3,9 +3,6 @@ public class HelloDevops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(" Hello Devops with Git & GitHub ");
-System.out.println(" I am the master ,I am adding one commande ");
-System.out.println(" I am the master ,I am adding my second commande ");
 System.out.println(" Hello Devops with Git & GitHub from developer");
 
 	}
