22a769c3c44df6ed63f146b5cb51c9113bd397c9
YousraMzouz <65360411+YousraMzouz@users.noreply.github.com>
2021-03-19 22:17:23 +0100

    Update HelloDevops.java
    change from the master

--- TP1-GIT/src/HelloDevops.java
+++ TP1-GIT/src/HelloDevops.java
@@ -4,6 +4,9 @@ public class HelloDevops {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(" Hello Devops with Git & GitHub ");
+		
+System.out.println(" I am the master ,I am adding one commande ");
+
 	}
 
 }
